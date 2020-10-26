df352501d994267060bc1e83b47f1a57b82653d0
cam-em <cmoreno.1117@gmail.com>
2020-10-26 17:59:00 -0500

    added styling, route logic, display routes

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../models");
+const axios = require("axios");
 
 // GET /pokemon - return a page with favorited Pokemon
-router.get('/', function(req, res) {
+router.get("/", function (req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  db.pokemon.findAll().then((favoritePokemon) => {
+    console.log(favoritePokemon);
+    res.render("favorites.ejs", { favorites: favoritePokemon });
+  });
+  //res.send("Render a page of favorites here");
+});
+
+router.get("/:id", (req, res) => {
+  const pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${req.params.id}`;
+  axios.get(pokemonUrl).then(function (apiResponse) {
+    console.log(apiResponse.data);
+    res.render("show.ejs", { data: apiResponse.data });
+  });
+  //res.send("Test");
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
-  // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+router.post("/", function (req, res) {
+  let pokemonName = req.body.name;
+  console.log(pokemonName);
+  db.pokemon
+    .findOrCreate({
+      where: {
+        name: pokemonName,
+      },
+      defaults: {
+        name: pokemonName,
+      },
+    })
+    .then(([createdPokemon, hasBeenCreated]) => {
+      console.log("Pokemon added:", createdPokemon);
+      console.log("Was added?:", hasBeenCreated);
+      res.redirect("/pokemon");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
